cb1d3ecd68e496492e93448428529c9fc0c09637
Vladislav Glinskiy <vglinskiy@cybervisiontech.com>
2018-01-10 17:39:55 +0200

    issue #13: Inaccurate 'Field avg' metric aggregation bug fixed

--- backend/src/main/java/com/mapr/grafana/plugin/model/timeseries/FieldAverageTimeSeries.java
+++ backend/src/main/java/com/mapr/grafana/plugin/model/timeseries/FieldAverageTimeSeries.java
@@ -13,7 +13,7 @@ public class FieldAverageTimeSeries extends AbstractGrafanaTimeSeries {
 
     private static final Logger log = LoggerFactory.getLogger(FieldAverageTimeSeries.class);
 
-    private Map<Long, Set<Datapoint>> intervalDatapoints = new HashMap<>();
+    private Map<Long, List<Datapoint>> intervalDatapoints = new HashMap<>();
     private String metricFieldPath;
 
     public FieldAverageTimeSeries(String target, String timeFieldPath, String metricFieldPath, long intervalMs) {
@@ -38,18 +38,20 @@ public class FieldAverageTimeSeries extends AbstractGrafanaTimeSeries {
             double value = document.getValue(metricFieldPath).getDouble();
             long timestamp = getDocumentTimestamp(document, timeFieldPath);
 
+            // First doc at interval
             if (this.datapoints.isEmpty() || timestamp - datapoints.get(datapoints.size() - 1).getTimestamp() >= intervalMs) {
                 Datapoint datapoint = new Datapoint(value, timestamp);
                 datapoints.add(datapoint);
 
-                Set<Datapoint> datapoints = new HashSet<>();
-                datapoints.add(datapoint);
+                List<Datapoint> datapoints = new ArrayList<>();
+                // add copy of datapoint, so the origin one can be modified safely
+                datapoints.add(new Datapoint(value, timestamp));
                 intervalDatapoints.put(timestamp, datapoints);
 
             } else if (!this.datapoints.isEmpty()) {
 
                 Datapoint last = datapoints.get(datapoints.size() - 1);
-                Set<Datapoint> existingIntervalDps = intervalDatapoints.get(last.getTimestamp());
+                List<Datapoint> existingIntervalDps = intervalDatapoints.get(last.getTimestamp());
                 existingIntervalDps.add(new Datapoint(value, timestamp));
 
                 OptionalDouble average = existingIntervalDps.stream().mapToDouble(Datapoint::getValue).average();
